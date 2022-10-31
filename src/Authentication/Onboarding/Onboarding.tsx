16750404f2d24b0b196af6991ca6ca035e8832d0
lehoanphuc <hoanphucle2001@gmail.com>
2022-10-31 23:40:08 +0700

    up code part1
    31/10/2022

---
+++ src/Authentication/Onboarding/Onboarding.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { View } from "react-native";
+
+const ComponentName = () => {
+  return <View style={{ flex: 1, backgroundColor: "cyan" }} />;
+};
+
+// eslint-disable-next-line import/no-default-export
+export default ComponentName;
